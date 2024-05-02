c99e086aa2c6cf6ea77c5bce5c9148d2dccb9518
fabiolorenzi <fabiolorenzi13975@gmail.com>
2024-05-02 11:00:34 +0100

    made brutalize function work

--- src/Checker.cpp
+++ src/Checker.cpp
@@ -26,6 +26,66 @@ int brutalize(Settings _settings)
                 successScript(temp);
                 return 0;
             }
+        } else {
+            for (int y {0}; y < charsOptions.size(); y++) {
+                brutePassword[1] = charsOptions[y];
+                if (brutePassword.size() == 2) {
+                    std::string temp = sumString(brutePassword);
+                    std::cout << temp << std::endl;
+                    if (checkPassword(brutePassword, _settings.userPassword) == true) {
+                        successScript(temp);
+                        return 0;
+                    }
+                } else {
+                    for (int z {0}; z < charsOptions.size(); z++) {
+                        brutePassword[2] = charsOptions[z];
+                        if (brutePassword.size() == 3) {
+                            std::string temp = sumString(brutePassword);
+                            std::cout << temp << std::endl;
+                            if (checkPassword(brutePassword, _settings.userPassword) == true) {
+                                successScript(temp);
+                                return 0;
+                            }
+                        } else {
+                            for (int w {0}; w < charsOptions.size(); w++) {
+                                brutePassword[3] = charsOptions[w];
+                                if (brutePassword.size() == 4) {
+                                    std::string temp = sumString(brutePassword);
+                                    std::cout << temp << std::endl;
+                                    if (checkPassword(brutePassword, _settings.userPassword) == true) {
+                                        successScript(temp);
+                                        return 0;
+                                    }
+                                } else {
+                                    for (int v {0}; v < charsOptions.size(); v++) {
+                                        brutePassword[4] = charsOptions[v];
+                                        if (brutePassword.size() == 5) {
+                                            std::string temp = sumString(brutePassword);
+                                            std::cout << temp << std::endl;
+                                            if (checkPassword(brutePassword, _settings.userPassword) == true) {
+                                                successScript(temp);
+                                                return 0;
+                                            }
+                                        } else {
+                                            for (int u {0}; u < charsOptions.size(); u++) {
+                                                brutePassword[5] = charsOptions[u];
+                                                if (brutePassword.size() == 6) {
+                                                    std::string temp = sumString(brutePassword);
+                                                    std::cout << temp << std::endl;
+                                                    if (checkPassword(brutePassword, _settings.userPassword) == true) {
+                                                        successScript(temp);
+                                                        return 0;
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 
@@ -43,11 +103,12 @@ std::string sumString(std::vector<char> _brutePassword)
 
 bool checkPassword(std::vector<char> _brutePassword, std::vector<char> _userPassword)
 {
-    bool isSame = true;
     for (int x {0}; x < _brutePassword.size(); x++) {
-        isSame = _brutePassword[x] == _userPassword[x];
+        if (_brutePassword[x] != _userPassword[x]) {
+            return false;
+        }
     }
-    return isSame;
+    return true;
 }
 
 void successScript(std::string value)
